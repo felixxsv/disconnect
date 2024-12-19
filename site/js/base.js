86f567bf8a27c9343d5863e0ec29befd96c82fb5
Felix <jimontd@icloud.com>
2024-12-19 20:41:59 +0900

    ver 0.0223 create base

--- site/js/base.js
+++ site/js/base.js
@@ -1,2 +1,10 @@
-let sidebar = document.getElementsByClassName("sidebar");
+let sidebar = document.querySelectorAll(".sidebar");
 
+sidebar.forEach(element => {
+    element.addEventListener("click", event => {
+        if (event.target.tagName === 'IMG') {
+            console.log("/site/html/" + event.target.alt + ".html");
+            // window.location.replace("/site/html/" + event.target.alt + ".html");
+        }
+    });
+});;
