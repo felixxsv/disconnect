4c181dca7dce161c18e0194d5fd0815a937c2cb5
Felix <jimontd@icloud.com>
2024-12-19 21:54:49 +0900

    ver 0.0024 create base

--- site/js/base.js
+++ site/js/base.js
@@ -1,4 +1,6 @@
 let sidebar = document.querySelectorAll(".sidebar");
+let sideList = document.querySelectorAll(".sideList");
+let sideListP = document.querySelectorAll(".sideList > p");
 
 sidebar.forEach(element => {
     element.addEventListener("click", event => {
@@ -8,3 +10,15 @@ sidebar.forEach(element => {
         }
     });
 });;
+
+sideList.forEach(element => {
+    element.addEventListener("click", event => {
+        if (event.target.tagName === 'IMG') {
+
+        }
+    });
+});;
+
+sideListP.forEach(p => {
+    p.textContent = `・${p.textContent}`;
+});
