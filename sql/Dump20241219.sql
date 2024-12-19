7709a20384eadf05c213a0daa3ac7d170fb2a89f
tasuku <226018@sapporo-mirai.ac.jp>
2024-12-19 15:21:04 +0900

    ver 0.0013 createSql

---
+++ sql/Dump20241219.sql
@@ -0,0 +1,249 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: disconnect
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `announce`
+--
+
+DROP TABLE IF EXISTS `announce`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `announce` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(8) NOT NULL,
+  `announce` varchar(1024) NOT NULL,
+  `annoucetime` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `announce_user_id_idx` (`user_id`),
+  CONSTRAINT `announce_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `announce`
+--
+
+LOCK TABLES `announce` WRITE;
+/*!40000 ALTER TABLE `announce` DISABLE KEYS */;
+/*!40000 ALTER TABLE `announce` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `badge`
+--
+
+DROP TABLE IF EXISTS `badge`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `badge` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(1024) NOT NULL,
+  `path` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `badge`
+--
+
+LOCK TABLES `badge` WRITE;
+/*!40000 ALTER TABLE `badge` DISABLE KEYS */;
+/*!40000 ALTER TABLE `badge` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `theme_id` int(11) NOT NULL,
+  `user_id` varchar(8) NOT NULL,
+  `comment` varchar(10000) NOT NULL,
+  `number` int(11) NOT NULL,
+  `post_time` datetime NOT NULL,
+  `like_count` int(11) NOT NULL,
+  PRIMARY KEY (`number`),
+  KEY `comment_theme_id_idx` (`theme_id`),
+  KEY `comment_user_id_idx` (`user_id`),
+  CONSTRAINT `comment_theme_id` FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`),
+  CONSTRAINT `commnet_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requesteduser`
+--
+
+DROP TABLE IF EXISTS `requesteduser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `requesteduser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(8) NOT NULL,
+  `client_id` varchar(8) NOT NULL,
+  `is_approve` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `requested_user_id_idx` (`user_id`),
+  CONSTRAINT `requested_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requesteduser`
+--
+
+LOCK TABLES `requesteduser` WRITE;
+/*!40000 ALTER TABLE `requesteduser` DISABLE KEYS */;
+/*!40000 ALTER TABLE `requesteduser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `title` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (0,'生徒'),(1,'学友会'),(2,'教師');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `theme`
+--
+
+DROP TABLE IF EXISTS `theme`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `theme` (
+  `id` int(11) NOT NULL,
+  `title` varchar(1024) NOT NULL,
+  `recruitment` varchar(1024) NOT NULL,
+  `user_id` varchar(8) NOT NULL,
+  `post_time` datetime NOT NULL,
+  `is_kind` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `theme_user_id_idx` (`user_id`),
+  CONSTRAINT `theme_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `theme`
+--
+
+LOCK TABLES `theme` WRITE;
+/*!40000 ALTER TABLE `theme` DISABLE KEYS */;
+/*!40000 ALTER TABLE `theme` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` varchar(8) NOT NULL,
+  `nickname` varchar(64) DEFAULT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(16) DEFAULT NULL,
+  `r_id` int(11) NOT NULL,
+  `path` varchar(256) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `r_id_idx` (`r_id`),
+  CONSTRAINT `r_id` FOREIGN KEY (`r_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('jimon000','jimon','jimon','226012',2,NULL,NULL),('tasu0124','tasuku','tasutasu','226018',0,NULL,NULL),('yang0001','yang','yang','226007',0,NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userbadge`
+--
+
+DROP TABLE IF EXISTS `userbadge`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `userbadge` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(8) NOT NULL,
+  `badge_id` int(11) NOT NULL,
+  `select_count` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `badge_user_id_idx` (`user_id`),
+  KEY `user_badge_id_idx` (`badge_id`),
+  CONSTRAINT `badge_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_badge_id` FOREIGN KEY (`badge_id`) REFERENCES `badge` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userbadge`
+--
+
+LOCK TABLES `userbadge` WRITE;
+/*!40000 ALTER TABLE `userbadge` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userbadge` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-19 15:11:23
