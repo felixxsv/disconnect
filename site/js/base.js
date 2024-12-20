4647b8281fea424abf8535f27cdf253c73051ebe
Felix <jimontd@icloud.com>
2024-12-20 11:22:23 +0900

    ver 0.1 create base admin

--- site/js/base.js
+++ site/js/base.js
@@ -3,12 +3,7 @@ let sideList = document.querySelectorAll(".sideList");
 let sideListP = document.querySelectorAll(".sideList > p");
 
 sidebar.forEach(element => {
-    element.addEventListener("click", event => {
-        if (event.target.tagName === 'IMG') {
-            console.log("/site/html/" + event.target.alt + ".html");
-            // window.location.replace("/site/html/" + event.target.alt + ".html");
-        }
-    });
+
 });;
 
 sideList.forEach(element => {
